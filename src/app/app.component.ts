c24f49025d23229072b3bcd386303156b819a603
Balazs Hetenyi <balazshetenyi82@gmail.com>
2022-10-07 13:54:30 +0100

    Added some event binding to recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+
+  loadedPage = 'recipe';
+
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
 }
